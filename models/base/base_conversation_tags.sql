1950967e5e100548e77a239309989ec18902c620
Michael Erasmus <michaelerasmus@gmail.com>
2018-11-21 10:44:01 -0800

    Add conversation base conversation tags model with nested fields

---
+++ models/base/base_conversation_tags.sql
@@ -0,0 +1,14 @@
+select
+    c.id as conversation_id
+    , array_agg(
+      struct(
+          md5(concat(cast(c.id as string), cast(t.index as string))) as id
+          , t.index
+          , t.tag
+        )
+    ) as tag
+from
+`buffer-data.helpscout.conversation` c
+inner join `buffer-data.helpscout.conversation_tag` t on t.conversation_id = c.id
+group by 1
+limit 1000
